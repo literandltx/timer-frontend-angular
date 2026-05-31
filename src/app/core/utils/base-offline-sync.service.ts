daa3d4ed7413cdf45bfe9170879dc9e503ffb3e7
literandltx <romanskovronn@gmail.com>
2026-05-31 17:37:52 +0300

    feat: refactor base offline sync

--- src/app/core/utils/base-offline-sync.service.ts
+++ src/app/core/utils/base-offline-sync.service.ts
@@ -1,24 +1,25 @@
-import { inject, signal } from '@angular/core';
+import { inject, signal, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {AuthService} from '../../feature/auth/auth.service';
+import { AuthService } from '../../feature/auth/auth.service';
 
-export abstract class BaseOfflineSyncService<TAction> {
+export abstract class BaseOfflineSyncService<TAction> implements OnDestroy {
   protected http = inject(HttpClient);
   protected authService = inject(AuthService);
+
+  protected abstract pingUrl: string;
+  protected abstract queueKey: string;
+  protected abstract syncQueue(): void | Promise<void>;
+
+  private pingInterval: any;
 
   isOnline = signal<boolean>(navigator.onLine);
   isSyncing = signal<boolean>(false);
 
-  protected abstract pingUrl: string;
-  protected abstract queueKey: string;
-
-  protected abstract syncQueue(): void | Promise<void>;
-
   constructor() {
-    window.addEventListener('online', () => this.checkBackendStatus());
-    window.addEventListener('offline', () => this.isOnline.set(false));
+    window.addEventListener('online', this.handleOnlineEvent);
+    window.addEventListener('offline', this.handleOfflineEvent);
 
-    setInterval(() => {
+    this.pingInterval = setInterval(() => {
       if (!this.authService.isAuthenticated()) return;
 
       if (!this.isOnline() || this.getQueue().length > 0) {
@@ -28,6 +29,17 @@ export abstract class BaseOfflineSyncService<TAction> {
 
     setTimeout(() => this.checkBackendStatus(), 0);
   }
+
+  ngOnDestroy() {
+    window.removeEventListener('online', this.handleOnlineEvent);
+    window.removeEventListener('offline', this.handleOfflineEvent);
+    if (this.pingInterval) {
+      clearInterval(this.pingInterval);
+    }
+  }
+
+  private handleOnlineEvent = () => this.checkBackendStatus();
+  private handleOfflineEvent = () => this.isOnline.set(false);
 
   protected getQueue(): TAction[] {
     return JSON.parse(localStorage.getItem(this.queueKey) || '[]');
@@ -41,7 +53,6 @@ export abstract class BaseOfflineSyncService<TAction> {
     const queue = this.getQueue();
     queue.push({ ...action, id: crypto.randomUUID() } as TAction);
     this.setQueue(queue);
-    this.syncQueue();
 
     if (this.authService.isAuthenticated()) {
       this.syncQueue();
