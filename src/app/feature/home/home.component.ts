04f39cb712889d5689f735ef27923a47fbd1fd49
literandltx <romanskovronn@gmail.com>
2026-06-02 14:47:39 +0300

    feat: refactor home component

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,18 +1,20 @@
-import {Component, OnInit, inject, effect, computed, signal} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-import {TimerComponent} from '../timer/timer/timer.component';
+import { Component, OnInit, inject, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { TimerComponent } from '../timer/timer/timer.component';
 
-import {LabelService} from '../labels/label.service';
-import {TimerSettingService} from '../timer/settings/timer-setting.service';
-import {TimerEntryService} from '../timer/entry/timer-entry.service';
-import {TitleBlinkerService} from './title-blinker.service';
+import { LabelService } from '../labels/label.service';
+import { TimerSettingService } from '../timer/settings/timer-setting.service';
+import { TimerEntryService } from '../timer/entry/timer-entry.service';
+import { TitleBlinkerService } from './title-blinker.service';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'ns-app-home',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterModule, TimerComponent],
+  providers: [HomeService],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -20,46 +22,15 @@ export class HomeComponent implements OnInit {
   public labelService = inject(LabelService);
   public settingService = inject(TimerSettingService);
   public entryService = inject(TimerEntryService);
+  public homeService = inject(HomeService);
   private blinkerService = inject(TitleBlinkerService);
-
-  activeLabelId = signal<number | undefined>(undefined);
-
-  constructor() {
-    const savedLabelId = localStorage.getItem('activeLabelId');
-
-    if (savedLabelId) {
-      this.activeLabelId.set(Number(savedLabelId));
-    }
-
-    effect(() => {
-      const labels = this.labelService.labels();
-      const currentId = this.activeLabelId();
-
-      if (labels.length > 0) {
-        const labelExists = labels.some(l => l.id === currentId);
-
-        if (currentId === undefined || !labelExists) {
-          this.activeLabelId.set(labels[0].id);
-        }
-      }
-    }, {allowSignalWrites: true});
-
-    effect(() => {
-      const currentId = this.activeLabelId();
-      if (currentId !== undefined) {
-        localStorage.setItem('activeLabelId', currentId.toString());
-      }
-    });
-  }
 
   activeLabelColor = computed(() => {
     const labels = this.labelService.labels();
-    if (labels.length === 0) return '#000000';
-
-    const id = this.activeLabelId();
+    const id = this.homeService.activeLabelId();
     const activeLabel = labels.find(l => l.id === id);
 
-    return activeLabel ? activeLabel.color : labels[0].color;
+    return activeLabel ? activeLabel.color : '#000000';
   });
 
   currentTimerSeconds = computed(() => {
@@ -93,10 +64,8 @@ export class HomeComponent implements OnInit {
   private saveHistory(durationSeconds: number) {
     const labels = this.labelService.labels();
     const fallbackLabel = labels.length > 0 ? labels[0].id : undefined;
-    const currentId = this.activeLabelId();
-    const labelExists = labels.some(l => l.id === currentId);
-    const finalLabelId = labelExists ? currentId : fallbackLabel;
+    const currentId = this.homeService.activeLabelId();
 
-    this.entryService.recordTimerFinish(durationSeconds, finalLabelId, fallbackLabel);
+    this.entryService.recordTimerFinish(durationSeconds, currentId, fallbackLabel);
   }
 }
