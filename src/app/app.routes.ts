2ec526dbcd1dd3bae74a3560d4b4029c4243f87d
literandltx <romanskovronn@gmail.com>
2026-07-05 13:41:04 +0300

    feat: renaming

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import {RegisterComponent} from './features/register/register.component';
 import {LoginComponent} from './features/login/login.component';
 import {LabelListComponent} from './features/labels/label-list.component';
 import {HistoryComponent} from './features/history/history.component';
-import {PresetComponent} from './features/settings/preset.component';
+import {PresetComponent} from './features/preset/preset.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
