d3045d015b51d966488bdd1fa5bab3e2d9fd5b5b
literandltx <romanskovronn@gmail.com>
2026-07-06 23:19:11 +0300

    feat: delete redundant pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './features/home/home.component';
-import {TimerListComponent} from './features/timers/timer-list.component';
 import {RegisterComponent} from './features/register/register.component';
 import {LoginComponent} from './features/login/login.component';
-import {LabelListComponent} from './features/labels/label-list.component';
 import {HistoryComponent} from './features/history/history.component';
 import {PresetComponent} from './features/preset/preset.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'preset', component: PresetComponent},
-  {path: 'timer-list', component: TimerListComponent},
-  {path: 'label-list', component: LabelListComponent},
   {path: 'history', component: HistoryComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
