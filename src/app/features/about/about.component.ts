70045b8aa9225166721bc145a361c6481f65d2e1
literandltx <139064216+literandltx@users.noreply.github.com>
2026-07-20 23:16:20 +0300

    feat: update user menu UI (#77)
    * feat: update user menu header ui
    
    * feat: add basic pages

---
+++ src/app/features/about/about.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ns-app-about',
+  standalone: true,
+  template: `
+    <div style="display: flex; justify-content: center; align-items: center; height: 100vh; width: 100%;">
+      <h1 style="margin: 0;">Coming soon</h1>
+    </div>
+  `
+})
+export class AboutComponent {
+}
