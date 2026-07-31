ec2cc67094251e0c0b633a415f2faf25da1704e4
literandltx <romanskovronn@gmail.com>
2026-07-31 18:32:06 +0300

    chore: decompose list-item page

--- src/app/shared/components/list-item/list-item.component.ts
+++ src/app/shared/components/list-item/list-item.component.ts
@@ -5,17 +5,7 @@ import { CommonModule } from '@angular/common';
   selector: 'ns-app-list-item',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div class="list-item" [class.is-active]="isActive">
-      <div class="item-info">
-        <ng-content select="[info]"></ng-content>
-      </div>
-
-      <div class="item-actions">
-        <ng-content select="[actions]"></ng-content>
-      </div>
-    </div>
-  `,
+  templateUrl: `./list-item.component.html`,
   styleUrls: ['./list-item.component.css']
 })
 export class ListItemComponent {
